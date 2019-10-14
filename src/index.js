6772b2e676d48d1ea9570599d449c2013c5d0e1b
Kris <kristian.claire.101@gmail.com>
2019-10-14 12:19:36 -0700

    adds comments for router in the index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route }  from "react-router-dom";
+import { BrowserRouter as Router }  from "react-router-dom"; // adds router for use
 
+
+// Here I have wrapped the app in the router.
 ReactDOM.render(
     <Router>
     <App />
